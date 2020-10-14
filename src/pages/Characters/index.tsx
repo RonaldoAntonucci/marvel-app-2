4aae2d26fb25e2f98417d2266aa50074d60b3831
Ronaldo Antonucci <ronaldo.antonuccivrb32@yahoo.com.br>
2020-10-14 15:21:58 -0300

    characters route

---
+++ src/pages/Characters/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Characters: React.FC = () => {
+  return <h1>Characters</h1>;
+};
+
+export default Characters;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,10 +4,12 @@ import { Switch } from 'react-router-dom';
 import Route from './Route';
 
 import Home from '../pages/Home';
+import Characters from '../pages/Characters';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={Home} />
+    <Route path="/characters/:id" component={Characters} />
   </Switch>
 );
 
